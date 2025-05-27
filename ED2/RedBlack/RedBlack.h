1b241a4af64aaf2f71d31105a7158bd99f0d1857
matheuspontes01 <matheus.spontes0406@gmail.com>
2025-05-26 22:05:13 -0300

    Arvore Rubro Negra em C

---
+++ ED2/RedBlack/RedBlack.h
@@ -0,0 +1,29 @@
+#ifndef REDBLACK_H
+#define REDBLACK_H
+#include <stdbool.h>
+
+// Color:
+// 0 - Black
+// 1 - Red
+
+typedef struct _node {
+    struct _node *left, *right, *p;
+    int color, key;
+} Node;
+
+
+typedef struct _tree {
+    Node* root;
+    Node* nil;
+} RedBlack;
+
+RedBlack* RedBlack_createTree();
+Node* createNode(RedBlack*, int);
+void RedBlack_insert(RedBlack*, int);
+void left_rotate(RedBlack*, Node*);
+void right_rotate(RedBlack*, Node*);
+void RedBlack_insertFixUp(RedBlack*, Node*);
+void RedBlack_pre(RedBlack*, Node*);
+void RedBlack_in(RedBlack*, Node*);
+
+#endif
---
+++ ED2/RedBlack/main.c
@@ -0,0 +1,30 @@
+#include "RedBlack.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_Tree(RedBlack* T) {
+    printf("Pre:\n");
+    printf(" KEY  |  COLOR  \n");
+    RedBlack_pre(T, T->root);
+
+    printf("In:\n");
+    printf(" KEY  |  COLOR  \n");
+    RedBlack_in(T, T->root);
+}
+
+int main() {
+    RedBlack* T = RedBlack_createTree();
+    if (!T) {
+        printf("Erro ao criar a arvore!\n");
+        free(T);
+        return 1;
+    }
+
+    int V[10] = {28, 30, 1, 5, 13, 8, 25, 12, 6, 2};
+
+    for (int i = 0; i < 10; i++) {
+        RedBlack_insert(T, V[i]);
+    }
+
+    print_Tree(T);
+}
